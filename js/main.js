9f2ab88b670954284d7df78551ecd33db72943fb
Your Name <you@example.com>
2023-03-21 16:13:50 +0100

    10 email

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+
+const listaEmail = document.getElementById('lista-email');
+    for (let i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => {
+          const email = response.data.response;
+          const li = document.createElement('li');
+          li.textContent = email;
+          listaEmail.appendChild(li);
+        })
+    }
